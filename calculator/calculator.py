1790bea5cb8d3f7b54bb89de653104b63a21bd3b
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2023-11-29 21:32:24 +0700

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,1 +1,51 @@
-
+multiply = 1division = 2addition = 3subtraction = 4end = 5action_end = 0
+while True:
+    try:
+        if action_end == end:
+            print('Калькулятор выключен')
+            break    except ValueError as e:
+        print("Всё заново")
+        break    except ZeroDivisionError as e:
+        print("Всё заново")
+        break    while True:
+        try:
+            number_1 = int(input('Введите первое число:'))
+            number_2 = int(input('Введите второе число:'))
+        except ValueError as e:
+            print("Всё заново")
+            break        except ZeroDivisionError as e:
+            print("Всё заново")
+            break        print('Выберите действие написав число: 1: умножить 2: разделить 3: сложение 4: вычитание')
+        try:
+            action = int(input())
+        except ValueError as e:
+            print("Всё заново")
+            break        except ZeroDivisionError as e:
+            print("Всё заново")
+            break        try:
+            if action == multiply:
+                print(number_1 * number_2)
+            if action == division:
+                print(number_1 / number_2)
+            if action == addition:
+                print(number_1 + number_2)
+            if action == subtraction:
+                print(number_1 - number_2)
+        except ValueError as e:
+            print("Всё заново")
+            break        except ZeroDivisionError as e:
+            print("Всё заново")
+            break        print('Выключить калькулятор?Да:5 Нет:6')
+        try:
+            action_end = int(input())
+        except ValueError as e:
+            print("Всё заново")
+            break        except ZeroDivisionError as e:
+            print("Всё заново")
+            break        try:
+            if action_end == end:
+                break        except ValueError as e:
+            print("Всё заново")
+            break        except ZeroDivisionError as e:
+            print("Всё заново")
+            break
