d276e7a5bda7d4983b2ae9ee7ce0f01024f10e61
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2023-11-29 21:26:36 +0700

    Update seacrh_B_in_txt.py

--- search_B_in_txt/seacrh_B_in_txt.py
+++ search_B_in_txt/seacrh_B_in_txt.py
@@ -1,3 +1,5 @@
-file_read=open('ex1.txt').read().replace('A','').replace('C','').rfind('B',0)if file_read > 0:
-    print(file_read+1)else:
+file_read=open('ex1.txt').read().replace('A','').replace('C','').rfind('B',0)
+if file_read > 0:
+    print(file_read+1)
+else:
     print('0')
