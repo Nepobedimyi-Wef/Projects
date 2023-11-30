3c658c9b17609163a5fcbbb7dce1de78af725d39
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2023-11-30 21:35:07 +0700

    Update my_decorator.py

--- my_decorator/my_decorator.py
+++ my_decorator/my_decorator.py
@@ -1,1 +1,9 @@
-
+def multiply(func):
+    def wrapper(a,b):
+        result = func(a,b)
+        return result*2
+    return wrapper
+@multiply
+def add_numbers(a,b):
+    return a+b
+print(add_numbers(25,21))
