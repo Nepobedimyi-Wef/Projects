b90f4d5e435f926ea3ec788308ca1def7eb4b08e
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2024-03-26 12:29:03 +0700

    Create dog.py

---
+++ dog.py
@@ -0,0 +1,43 @@
+class Person:
+    def __init__(self, name, age, color, breed, weight):
+        self.__name = name  # устанавливаем имя
+        self.__age = age  # устанавливаем возраст
+        self.__color = color # устанавливаем цвет
+        self.__breed = breed # устанавливаем породу
+        self.__weight = weight # устанавливаем вес
+
+    # сеттер для установки возраста
+    def set_age(self, age):
+        if 0 < age <= 50:
+            self.__age = age
+        else:
+            print("Недопустимый возраст")
+
+    # сеттер для установки веса
+    def set_weight(self, weight):
+        if 1 < weight <= 100:
+            self.__weight = weight
+        else:
+            print("Недопустимый вес")
+
+    # геттер для получения возраста
+    def get_age(self):
+        return self.__age
+
+    # геттер для получения имени
+    def get_name(self):
+        return self.__name
+
+    def print_person(self):
+        if 0 < Age <= 50 and 1 < Weight <= 100:
+            print(f"Имя: {self.__name}\tВозраст: {self.__age}\tЦвет: {self.__color}\tПорода: {self.__breed}\tВес: {self.__weight}")
+
+Name = input('Введите имя собаки: ')
+Age = int(input('Введите возраст собаки: '))
+Color = input('Введите цвет собаки: ')
+Breed = input('Введите породу собаки: ')
+Weight = int(input('Введите вес собаки: '))
+tom = Person(Name, '', Color, Breed, '')
+tom.set_age(Age)
+tom.set_weight(Weight)
+tom.print_person()
