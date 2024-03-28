a0b701b19eef8c3ade32c641dca4c803f8320df3
Nepobedimyi-Wef <151131838+Nepobedimyi-Wef@users.noreply.github.com>
2024-03-28 15:40:51 +0700

    Create dog.py

---
+++ dog/dog.py
@@ -0,0 +1,23 @@
+class Animal:
+
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    def display_name(self):
+        print(f"Name: {self.__name}",f"\nAge: {self.__age} ")
+
+
+class Dog(Animal):
+
+    def say(self):
+        print(f"{self.name} say: Bark")
+
+
+tom = Dog("Rex",10)
+tom.display_name()
+tom.say()
